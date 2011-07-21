6f7c0261c93c6e6e9de0fb66c04f0a9f1f1d764a
Keeran Raj Hawoldar <keeran@gmail.com>
2011-07-21 19:54:14 +0100

    demo bots

---
+++ NervousDuck.rb
@@ -0,0 +1,15 @@
+require 'rrobots'
+
+class NervousDuck
+  include Robot
+  
+  def tick events
+    turn_radar 1 if time == 0
+    turn_gun 30 if time < 3   
+
+    accelerate 1
+    turn 2
+    fire 3
+  end
+
+end
